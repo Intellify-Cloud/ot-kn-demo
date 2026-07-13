5e4e256f070183aa24f3fb0f1643d32757e1e7a8
Pieter <oosthuizenp@me.com>
2026-07-13 23:03:24 +0200

    Remove TeamSection from HomeView component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import HeroSection from "../components/sections/HeroSection.vue";
 import AboutSection from "../components/sections/AboutSection.vue";
-import TeamSection from "../components/sections/TeamSection.vue";
 import StepsSection from "../components/sections/StepsSection.vue";
 import ContactSection from "../components/sections/ContactSection.vue";
 </script>
@@ -10,7 +9,6 @@ import ContactSection from "../components/sections/ContactSection.vue";
   <div>
     <HeroSection />
     <AboutSection />
-    <TeamSection />
     <StepsSection />
     <ContactSection />
   </div>
